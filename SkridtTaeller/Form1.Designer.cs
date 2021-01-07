d63ab2c0150e41183a2bd2da7314cbc92edb5165
Maximus <46528266+SmellyB3lly@users.noreply.github.com>
2021-01-07 12:40:43 +0100

    New Icon

--- SkridtTaeller/Form1.Designer.cs
+++ SkridtTaeller/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace SkridtTaeller
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.SuspendLayout();
             // 
             // Form1
@@ -36,6 +37,7 @@ namespace SkridtTaeller
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(684, 565);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
